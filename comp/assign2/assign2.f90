b98add68be8d88773a33ba1d5605d71e08f817b9
rylanjardine <rjar0001@student.monash.edu>
2020-04-19 18:09:53 +1000

    first part of assign 2

---
+++ comp/assign2/assign2.f90
@@ -0,0 +1,65 @@
+program assign
+  use integrator
+  implicit none
+  real :: x(2),v(2),a(2),r,t,l,u,dl
+  integer :: i,j
+  real,parameter :: dt=0.05
+
+
+
+
+  t=0.
+  x(1)=1.-e
+  x(2)=0.
+
+  v(1)=0.
+  v(2)=sqrt((1+e)/(1-e))
+
+
+  r=sqrt(x(1)**2+x(2)**2)
+  a(1)=-G*M*x(1)/r**3
+  a(2)=-G*M*x(2)/r**3
+  l=x(1)*v(2)-x(2)*v(1)
+  dl=l
+  u=0.5*(v(1)**2+v(2)**2)-1./r
+
+
+  print*,'Please select Leapfrog (1) or Runge Kutta (2)'
+  read*,j
+  ! if (j==1) then
+  !   call tim(x,v,a,dt)
+  ! else if (j==2) then
+  !   call rung(x,v,a,dt)
+  ! else
+  !   print*, 'Default = Leapfrog'
+  !   call tim(x,v,a,dt)
+  ! endif
+
+  open(1,file='param.dat',status='replace',action='write')
+  write(1,*)'# x y v_x v_y a_x a_y t l u dl'
+  write(1,*) x(1),x(2),v(1),v(2),a(1),a(2),t,l,u,l-dl
+  do i=1,5000
+    t=t+dt
+    !only output specific timesteps
+    ! if (t>tprint) then
+    !   !update output parameters
+    !   ifile=ifile+1
+    !   tprint=(ifile+1)*dtout
+    !   call output(n,x,v,h,nx,rho,m,p,cs,a,t,ifile,ng,u,du)
+    ! end if
+    !timestepping
+    if (j==1) then
+      call tim(x,v,a,dt)
+    else if (j==2) then
+      call rung(x,v,a,dt)
+    else
+      call tim(x,v,a,dt)
+    endif
+    ! call tim(x,v,a,dt)
+    l=x(1)*v(2)-x(2)*v(1)
+    u=0.5*(v(1)**2+v(2)**2)-1./r
+  write(1,*) x(1),x(2),v(1),v(2),a(1),a(2),t,l,u,l-dl
+  enddo
+  close(1)
+
+end program
---
+++ comp/assign2/integrate.f90
@@ -0,0 +1,91 @@
+module integrator
+  implicit none
+  real,parameter :: e=0.7,G=1.,m=1.
+  
+
+contains
+  subroutine tim(x,v,a,dt)
+    real,intent(inout) :: x(2),v(2)
+    real, intent(out) :: a(2)
+    real,intent(in) :: dt
+    real :: r,a0(2)
+
+        a0=a
+
+
+        !update x and v and u half-steps
+        x=x+dt*v+0.5*(dt)**2*a0
+
+        v=v+dt*a
+
+        !update a and du
+
+        r=sqrt(x(1)**2+x(2)**2)
+        a(1)=-G*M*x(1)/r**3
+        a(2)=-G*M*x(2)/r**3
+
+        !update final step of v and u
+        v=v+0.5*dt*(a-a0)
+  end subroutine
+
+
+  subroutine rung(x,v,a,dt)
+    real,intent(inout) :: x(2),v(2)
+    real, intent(out) :: a(2)
+    real,intent(in) :: dt
+    real :: r,a0(2),h
+    real :: k0(2),k1(2),k2(2),k3(2),l0(2),l1(2),l2(2),l3(2)
+
+
+    ! r=sqrt(x(1)**2+x(2)**2)
+    !a=-gmx/r**3
+    !set x1=x, x2=v, thus
+    ! dx1/dt=f1(t,x1,x2)=x2=v
+    !dx2/dt=dv/dt=-GMx1/r**3
+    r=sqrt(x(1)**2+x(2)**2)
+
+
+
+    k0=dt*f1(x,v)
+    l0=dt*f2(x,v)
+    ! r=sqrt(x(1)**2+x(2)**2)
+    k1=dt*f1(x+0.5*k0,v+0.5*l0)
+    l1=dt*f2(x+0.5*k0,v+0.5*l0)
+    ! r=sqrt(x(1)**2+x(2)**2)
+    k2=dt*f1(x+0.5*k1,v+0.5*l1)
+    l2=dt*f2(x+0.5*k1,v+0.5*l1)
+    ! r=sqrt(x(1)**2+x(2)**2)
+    k3=dt*f1(x+k2,v+l2)
+    l3=dt*f2(x+k2,v+l2)
+    ! r=sqrt(x(1)**2+x(2)**2)
+
+    x=x+1./6.*(k0+2.*k1+2.*k2+k3)
+    v=v+1./6.*(l0+2.*l1+2.*l2+l3)
+
+    a(1)=-G*M*x(1)/r**3
+    a(2)=-G*M*x(2)/r**3
+
+  end subroutine
+
+
+    function f1(x,v)
+      real,intent(in) :: x(2),v(2)
+      real, dimension(2) :: f1
+
+      f1(:)=v(:)
+
+    end function
+
+
+    function f2(x,v)
+      real,intent(in) :: x(2),v(2)
+      ! real,intent(in) :: r
+      real, dimension(2) :: f2
+      real :: r
+
+      r=sqrt(x(1)**2+x(2)**2)
+      f2(:)=-G*m*x(:)/r**3
+
+  end function
+
+end module
